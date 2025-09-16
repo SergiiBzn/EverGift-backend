6c3111eebaaa18788c91787850c18c81afdcdd69
Sergii Buzun <sergiibuzun@gmail.com>
2025-09-16 16:21:23 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import chalk from 'chalk';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import './db/index.js';
+import { errorHandler } from './middlewares/index.js';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json(), cookieParser());
+app.use(cors());
+
+app.get('/health', async (_req, res) => {
+  res.json({ msg: 'Running' });
+});
+
+app.use('/{*splat}', (req, _res) => {
+  throw new Error(`URL unavailable; you used ${req.originalUrl}`, {
+    cause: 404,
+  });
+});
+
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(chalk.bgGreen(` Server listening on port ${port} `));
+});
