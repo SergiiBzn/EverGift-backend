8af87893916105b5dee5a3307f4572ec9f98ff0d
Sergii Buzun <sergiibuzun@gmail.com>
2025-09-18 16:38:12 +0200

    user model

--- models/User.js
+++ models/User.js
@@ -1,3 +1,34 @@
-import { Schema, model } from "mongoose";
+import { Schema, model } from 'mongoose';
 
-const userSchema = new Schema({});
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+
+  profil: {
+    username: {
+      type: String,
+      default: '',
+    },
+  },
+  wishList: {
+    type: Array,
+    default: [],
+  },
+  giftHistory: [
+    {
+      giftName: String,
+      from: { type: mongoose.Schema.Types.ObjectId, ref: 'Contact' },
+      fromName: String,
+      recievedDate: { type: Date, required: true },
+    },
+  ],
+});
+
+const User = model('user', userSchema);
+export default User;
