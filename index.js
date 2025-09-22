302695c2b98aab59eeaaf05f084b842cf9340e31
arnaudbrice <mephisteur@gmail.com>
2025-09-22 13:45:14 +0200

    add auth Controller logic and Authenticate middleware

--- index.js
+++ index.js
@@ -4,10 +4,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import "./db/index.js";
 import { errorHandler } from "./middlewares/index.js";
-<<<<<<< HEAD
-=======
 import { userRouter, contactRouter } from "./routes/index.js";
->>>>>>> 276e2cd3154d9aaf9288e3c14cde2f2f138ccb1d
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -19,19 +16,17 @@ app.get("/health", async (_req, res) => {
   res.json({ msg: "Running" });
 });
 
-<<<<<<< HEAD
-=======
 app.use("/users", userRouter);
 app.use("/contacts", contactRouter);
 
->>>>>>> 276e2cd3154d9aaf9288e3c14cde2f2f138ccb1d
 app.use("/{*splat}", (req, _res) => {
   throw new Error(`URL unavailable; you used ${req.originalUrl}`, {
-    cause: 404
+    cause: 404,
   });
 });
 
 app.use(errorHandler);
+
 app.listen(port, () => {
   console.log(chalk.bgGreen(` Server listening on port ${port} `));
 });
--- models/User.js
+++ models/User.js
@@ -1,28 +1,5 @@
 import { Schema, model } from "mongoose";
 
-<<<<<<< HEAD
-//********** ReceivedGift Schema **********/
-const receivedGiftSchema = new Schema({
-  ownerId: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
-  contactType: {
-    type: String,
-    enum: ['user', 'custom'],
-    required: true,
-  },
-  gift: { type: Schema.Types.ObjectId, ref: 'Gift' },
-  from: [{ type: Schema.Types.ObjectId, ref: 'Contact' }],
-  fromName: { type: [String], default: [] }, // custom
-});
-
-const ReceivedGift = model('ReceivedGift', receivedGiftSchema);
-
-//********** User Schema **********/
-=======
->>>>>>> 276e2cd3154d9aaf9288e3c14cde2f2f138ccb1d
 const userSchema = new Schema({
   email: {
     type: String,
