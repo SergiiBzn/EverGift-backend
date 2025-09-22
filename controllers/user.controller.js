9c0df0529a07ef59b1ce12ac23b32fa3e2cce7f6
arnaudbrice <mephisteur@gmail.com>
2025-09-22 16:21:10 +0200

    add user controller for the profile

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,25 +1,25 @@
-import { User } from "../models/index.js";
+import  User  from "../models/index.js";
 
 // profile update and wishList update
 //********** PUT /users/profile **********
-export const updateUserProfile = async(req,res) => {
-  const {name,avatar,birthday,tags}=req.body;
-  const userId=req.userId;
-  const user=await User.findByIdAndUpdate(userId,{name,avatar,birthday,tags},{new:true});
-  if(!user){
-    throw new Error("User not found",{cause:404})
+export const updateUserProfile = async (req, res) => {
+  const { name, avatar, birthday, tags } = req.body;
+  const userId = req.userId;
+  const user = await User.findByIdAndUpdate(userId, { name, avatar, birthday, tags }, { new: true });
+  if (!user) {
+    throw new Error("User not found", { cause: 404 });
   }
   res.json(user);
-}
+};
 
 
 //********** PUT /users/wishList **********
-export const updateUserWishList = async(req,res) => {
-  const {wishItem}=req.body;
-  const userId=req.userId;
-  const user=await User.findByIdAndUpdate(userId,{$push:{wishList:wishItem}},{new:true,runValidators:true});
-  if(!user){
-    throw new Error("User not found",{cause:404})
+export const updateUserWishList = async (req, res) => {
+  const { wishItem } = req.body;
+  const userId = req.userId;
+  const user = await User.findByIdAndUpdate(userId, { $push: { wishList: wishItem } }, { new: true, runValidators: true });
+  if (!user) {
+    throw new Error("User not found", { cause: 404 });
   }
   res.json(user);
 }
