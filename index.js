2926aad9ab30e2d98d6b7171aeb37b08ae4dfb9a
arnaudbrice <mephisteur@gmail.com>
2025-09-24 13:59:32 +0200

    add cors

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/** @format */
+
 import express from "express";
 import chalk from "chalk";
 import cors from "cors";
@@ -9,8 +11,8 @@ import { userRouter, contactRouter, authRouter } from "./routers/index.js";
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
 app.use(express.json(), cookieParser());
-app.use(cors());
 
 app.get("/health", async (_req, res) => {
   res.json({ msg: "Running" });
