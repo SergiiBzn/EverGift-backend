d5023c2bdd67fb20845e6c211fe042eedbf40e3a
Yan <y.y.erica@hotmail.com>
2025-09-25 11:23:45 +0200

    Update auth.controller.js

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -87,8 +87,8 @@ export const login = async (req, res) => {
 export const logout = async (req, res) => {
   res.clearCookie('token', {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'lax',
+   secure: process.env.NODE_ENV === 'production',
+    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
   });
   res.status(204).json({
     message: 'Logged Out Successfully',
