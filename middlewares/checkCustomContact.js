e7d0bdb090599eb2d9c1c06dd105663d78c6bd37
Yan Yang <y.y.erica@hotmail.com>
2025-09-29 23:27:45 +0200

    modified contacts router with slug

--- middlewares/checkCustomContact.js
+++ middlewares/checkCustomContact.js
@@ -1,12 +1,13 @@
 import { Contact } from "../models/index.js";
 const checkCustomContact = async (req, res, next) => {
-  const { contactId } = req.params;
+  const { contactSlug } = req.params;
   const ownerId = req.userId;
-  const contact = await Contact.findOne({ _id: contactId, ownerId });
+  const contact = await Contact.findOne({ slug: contactSlug, ownerId });
   if (!contact) throw new Error("Contact not found", { cause: 404 });
   if (contact.contactType !== "custom")
     throw new Error("Operation not allowed for linked user", { cause: 403 });
   req.contact = contact;
+
   next();
 };
 export default checkCustomContact;
--- middlewares/index.js
+++ middlewares/index.js
@@ -1,5 +1,8 @@
-export { default as errorHandler } from './errorHandler.js';
-export { default as checkCustomContact } from './checkCustomContact.js';
-export { default as authenticate } from './authenticate.js';
-export { default as validate } from './validate.js';
-export { default as checkContact } from './checkContact.js';
+export { default as errorHandler } from "./errorHandler.js";
+export { default as checkCustomContact } from "./checkCustomContact.js";
+export { default as authenticate } from "./authenticate.js";
+export { default as validate } from "./validate.js";
+export { default as checkContact } from "./checkContact.js";
+export { default as parseCustomProfile } from "./parseCustomProfile.js";
+export { default as uploadFile } from "./imageFileUpload.js";
+export { restructureContactBody } from "./restructureContactBody.js";
